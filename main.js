14b5f074caee9b0a5622728e1fd49621d5d58ec3
Victor Hugo Scofano de Carvalho <112015199+vscofano@users.noreply.github.com>
2024-08-23 17:19:09 -0300

    publicando

---
+++ main.js
@@ -0,0 +1,286 @@
+let market_place = [
+    {
+        "titulo": "Americanas/B2W",
+         "breve_descricao":"Para vender na Americanas Marketplace (Americanas, Shoptime ou Submarino) o lojista pode fazer o cadastro no canal ou utilizar o Olist. Uma vez atendidos os pré-requisitos é possível fechar o acordo, cadastrar os produtos, aguardar a publicação e gerenciar pedidos e o pós-venda.",
+        "tabela": {
+            "taxa_1": "Taxas: Comissões variam de 12% a 16%, dependendo da categoria.",
+            "taxa_2": "",
+            "envio": "Opções: B2W Entrega integra transportadoras e Correios.",
+            "frete": "Possibilidade de frete grátis.",
+            "cpf": "Documentação:Necessário CNPJ. Pessoas físicas não podem se cadastrar diretamente.",
+            "cnpj": "Registro: Cadastro de empresa com CNPJ é obrigatório.",
+            "nf": "Emissão de Nota Fiscal é obrigatória.",
+            "obs": "Produtos de mostruário podem ser vendidos, mas devem ser claramente descritos e identificados como tais.",
+            "link": "https://olist.com/blog/pt/como-vender-mais/em-marketplaces/como-vender-na-americanas-marketplace-com-olist/"
+        }
+    },
+    {
+        "titulo": "Amazon",
+        "breve_descricao": "Para vender na Amazon, você precisa se cadastrar como vendedor profissional e se inscrever no programa Amazon FBA. Depois, você cataloga os seus produtos e os envia para os armazéns determinados. Ao final, você recebe o valor do pedido, com as taxas de comissão e serviços descontadas.A Amazon oferece dois planos de vendas: Profissional e Individual. O plano Profissional tem uma mensalidade de R$19,00 a partir do 13º mês, mas é gratuito durante o primeiro ano. O plano Individual não tem mensalidade, mas cobra uma taxa de R$2,00 por item vendido. Em ambos os planos, os vendedores pagam uma comissão que pode variar entre 8% e 15% do preço de venda de acordo com a categoria do produto.Os pagamentos são realizados via transferência bancária 60 dias após as vendas, diretamente na conta bancária cadastrada na sua conta de associado.Você pode usar a ferramenta Seller Central para gerenciar a sua loja, controlar vendas, atualizar o portfólio e monitorar métricas",
+        "tabela": {
+            "taxa_1": "Taxas: Comissões de 8% a 20%, dependendo da categoria.",
+            "taxa_2": "Taxa de R$ 2,00 por item vendido no plano individual.",
+            "envio": "Opções: Envio direto pelo vendedor ou FBA (Fulfillment by Amazon), onde a Amazon cuida da logística.",
+            "frete": "Frete: Gerenciado pela Amazon ou pelo próprio vendedor.",
+            "cpf": "Documentação: CPF pode ser utilizado. Dados bancários são necessários para recebimento.",
+            "cnpj": "Registro: Não é necessário CNPJ.",
+            "nf": "Não é exigida inicialmente, mas é recomendável verificar as normas locais.",
+            "obs": "Certifique-se de informar claramente que o item é um mostruário, com uma descrição detalhada e condições do produto.",
+            "link": "https://venda.amazon.com.br/sellerblog/como-vender-na-amazon--primeiros-passos"
+        }
+    },
+    {
+        "titulo": "Dafiti",
+        "breve_descricao": "A Dafiti não cobra nada pela criação da sua loja, apenas uma comissão pelas vendas feitas dentro do marketplace, que varia de acordo com a negociação feita com você",
+        "tabela": {
+            "taxa_1": "Comissão de 16% sobre o valor do produto.",
+            "envio": "Dafiti Full: Armazenamento e envio gerenciados pela Dafiti.",
+            "frete": "Possibilidade de envio direto pelo vendedor..",
+            "cpf": "Não aceita Pessoa Física.",
+            "cnpj": "Necessário CNPJ. Não é possível para pessoas físicas vender diretamente.",
+            "nf": "Requer Nota Fiscal para todas as vendas.",
+            "obs": "Público-alvo focado em moda.O CNPJ é importante para transmitir credibilidade aos clientes e para estar totalmente regularizado com as tributações. ",
+            "link": "https://www.dafiti.com.br/quero-vender-na-dafiti/"
+        }
+    },
+    {
+        "titulo": "Elo7",
+        "breve_descricao":"Esse, é um marketplace voltado para produtos artesanais e nao permite produtos industrializados Assim, o público é segmentado e a competição com as grandes corporações é reduzida, se não excluída. O Elo7 é o maior marketplace dessa categoria na América Latina e conta com milhares de visitantes.",
+        "tabela": {
+            "taxa_1": "Taxas: Comissão de 12% por venda.",
+            "taxa_2": "Plano Plus com mensalidade de R$ 59,90 reduz a comissão para 10%.",
+            "envio": "Opções: Integração com Correios e transportadoras.",
+            "frete": "Calculado automaticamente.",
+            "cpf": "Documentação: CPF é suficiente. Dados bancários são necessários",
+            "cnpj": "CNPJ não é obrigatório.",
+            "nf": "Não é exigida, mas pode ser recomendada para maior transparência",
+            "obs": "A plataforma é voltada para produtos personalizados e artesanais, e mostruários devem ser bem descritos",
+            "link": "https://tray.com.br/escola/como-vender-no-elo7/"
+        }
+    },
+    {
+        "titulo": "Magalu Marketplace",
+        "breve_descricao": "Para vender no Magalu Marketplace, você pode: Acessar o site do Magalu Marketplace e fazer um cadastro rápido. Escolher se utiliza alguma integradora ou fará o cadastro de produtos de forma manual. Aguardar a avaliação do Magazine Luiza em até 48 horas para começar a vender. Cadastrar produtos com poucos cliques. Emitir notas fiscais de forma descomplicada. Criar cupons de desconto. Divulgar os produtos nas principais redes sociais. Cadastrar os clientes e gerenciar o banco de dados de forma unificada. O Magalu Marketplace cobra apenas comissão por venda realizada, e o valor varia entre 10% e 20%, dependendo da categoria do produto. Depois de efetuada a transação e descontadas essas tarifas, o Magalu repassa o valor da venda semanalmente ou mensalmente, conforme sua preferência.",
+        "tabela": {
+            "taxa_1": "Taxas: Comissões variam de 10% a 20%, dependendo da categoria.",
+            "envio": "Opções: Magalu Entregas oferece integração com transportadoras e Correios.",
+            "frete": "Frete: Possibilidade de frete grátis.",
+            "cpf": "Não aceita Pessoa Física.",
+            "cnpj": "Necessário CNPJ. Não é possível para pessoas físicas vender diretamente.",
+            "nf": "Emissão de Nota Fiscal é necessária.",
+            "obs": "Produtos de mostruário podem ser vendidos, mas devem ser claramente descritos e identificados como tais.",
+            "link": "https://universo.magalu.com/"
+        }
+    },
+    {
+        "titulo": "Mercado Livre",
+        "breve_descricao": "Para vender no Mercado Livre, você precisa criar uma conta na plataforma como vendedor. Depois, pode anunciar produtos, configurar as condições de venda e clicar em Anunciar. Não há custo para criar anúncios, mas você paga uma tarifa ao concluir cada venda. A tarifa depende do tipo de anúncio (Clássico ou Premium), do valor e da categoria do produto. Por exemplo, um anúncio Clássico tem uma tarifa de 10% a 14% por categoria, mais R$ 6 por unidade vendida de produtos com valor menor que R$ 79. Um anúncio Premium tem uma tarifa de 15% a 19% por categoria, mais R$ 6 por unidade vendida de itens abaixo de R$ 79, e também permite parcelamento de até 18x sem juros. Depois de concluir a venda, você recebe o dinheiro na sua conta do Mercado Pago. Se você entregou o produto com o Mercado Envios, o valor é creditado 8 dias após a entrega. Para produtos usados e/ou vendedores sem reputação, o prazo é de 12 dias após a entrega. Se você é MercadoLíder, o dinheiro fica disponível 11 dias após o comprador receber o produto. Se você não é MercadoLíder, o dinheiro fica disponível em 28 dias.",
+        "tabela": {
+            "taxa_1": "Taxas: Comissões variam de 12% a 19%, dependendo da categoria.",
+            "taxa_2": "Taxa Fixa: R$ 5,00 para vendas abaixo de R$ 79,00 no plano Clássico.",
+            "envio": "Mercado Envios integra diversas transportadoras e Correios",
+            "frete": "Frete:Possibilidade de oferecer frete grátis, com subsídio parcial pelo vendedor",
+            "cpf": "Documentação: CPF é suficiente para pessoas físicas.",
+            "cnpj": "Não é necessário CNPJ",
+            "nf": "Não é obrigatória para pessoas físicas, mas é recomendada para maior transparência e para manter a conformidade com as normas fiscais",
+            "obs": "Descrição do Produto: É importante especificar claramente o estado do item, incluindo se é um mostruário ou amostra",
+            "link": "https://vendedores.mercadolivre.com.br/nota/cinco-passos-para-comecar-seu-negocio-no-mercado-livre/"
+        }
+    },
+    {
+        "titulo": "Shopee",
+        "breve_descricao": "Para vender na Shopee, um marketplace de comércio eletrónico de origem asiática, os vendedores devem registar-se no site da Shopee: Fazer o registo como vendedor com CPF ou CNPJ, Personalizar o layout da loja, Adicionar meios de envio, Registar uma conta bancária para pagamento, Adicionar os produtos. A Shopee cobra uma comissão padrão de 14% sobre o valor total do produto, mais uma taxa fixa. A partir de julho de 2024, a taxa por item vendido aumentou de R$ 3 para R$ 4, exceto para produtos que custam menos de R$ 8. Os vendedores também podem participar em campanhas de destaque da Shopee, o que acarreta um serviço adicional de participação de 2% durante o período da campanha. Os vendedores também podem optar pelo Shopee Entrega Direta, no qual são responsáveis por todo o processo de entrega",
+        "tabela": {
+            "taxa_1": "Taxas: Comissões variam de 5% a 18%, dependendo da categoria",
+            "taxa_1": "Adicional de 6% a 12% para o programa de frete grátis",
+            "envio": "Opções: Opções: Shopee Entrega integra diversas transportadoras.",
+            "frete": "Frete grátis pode ser oferecido, com subsídio parcial pelo vendedor",
+            "cpf": "Documentação: CPF é suficiente. Dados bancários são necessários para recebimento",
+            "cnpj": "Não é necessário CNPJ",
+            "nf": "Não é obrigatória inicialmente, mas pode ser exigida dependendo do volume de vendas.",
+            "obs": "PÉ importante descrever o estado do produto claramente, incluindo se é um mostruário ou amostra.",
+            "link": "https://seller.shopee.com.br/edu/article/13388/como-comeco-a-vender-na-shopee"
+        }
+    },
+];
+document.addEventListener('DOMContentLoaded', function() {
+    const botao_home = document.getElementById("btn_home");
+    const botao_1 = document.getElementById("btn_1");
+    const botao_2 = document.getElementById("btn_2");
+    const botao_3 = document.getElementById("btn_3");
+    const botao_4 = document.getElementById("btn_4");
+    const botao_5 = document.getElementById("btn_5");
+    const botao_6 = document.getElementById("btn_6");
+    const botao_7 = document.getElementById("btn_7");
+    const botao_8 = document.getElementById("btn_8");
+    const content_graphs = document.getElementById("graphs");
+    const content_main = document.getElementById("main");
+    const menu = document.getElementById('menu')
+    const imagem = document.getElementById('logo');
+    const descricao = document.getElementById('descricao');
+    const titulo = document.querySelectorAll('.titulo');
+    const taxa_1 = document.getElementById('taxa_1');
+    const taxa_2 = document.getElementById('taxa_2');
+    const envio = document.getElementById('envio');
+    const frete = document.getElementById('frete');
+    const cpf = document.getElementById('cpf');
+    const cnpj = document.getElementById('cnpj');
+    const observacao = document.getElementById('observacao')
+    const nf = document.getElementById('nf');
+    const link = document.getElementById('link');
+
+    function handleButtonClick() {
+        menu.style.display = 'none'; 
+        content_main.style.display = 'block';
+        content_graphs.style.display = 'none'
+    }
+    function handleButtonClick_graph() {
+        menu.style.display = 'none'; 
+        content_main.style.display = 'none';
+        content_graphs.style.display = 'block'
+    }
+    function handleButtonClick_home() {
+        menu.style.display = 'block'; 
+        content_main.style.display = 'none';
+        content_graphs.style.display = 'none'
+    }
+
+    botao_home.addEventListener('click',function(){
+        handleButtonClick_home();
+    });
+
+    botao_1.addEventListener('click', function() {
+        imagem.src = 'image/americanas.jpg';
+        titulo.forEach(function(titulo){
+            titulo.textContent = 'Americanas/B2W';
+        });
+        descricao.textContent = market_place[0].breve_descricao;
+        taxa_1.textContent = market_place[0].tabela.taxa_1;
+        taxa_2.textContent = market_place[0].tabela.taxa_2;
+        envio.textContent = market_place[0].tabela.envio;
+        frete.textContent = market_place[0].tabela.frete;
+        cpf.textContent = market_place[0].tabela.cpf;
+        cnpj.textContent = market_place[0].tabela.cnpj;
+        nf.textContent = market_place[0].tabela.nf;
+        link.href = market_place[0].tabela.link;
+        observacao.textContent = market_place[0].tabela.obs;
+        handleButtonClick();
+
+    });
+
+
+    botao_2.addEventListener('click', function() {
+        imagem.src = 'image/amz.jpg';
+        titulo.forEach(function(titulo){
+            titulo.textContent = 'Amazon';
+        });
+        descricao.textContent = market_place[1].breve_descricao;
+        taxa_1.textContent = market_place[1].tabela.taxa_1;
+        taxa_2.textContent = market_place[1].tabela.taxa_2;
+        envio.textContent = market_place[1].tabela.envio;
+        frete.textContent = market_place[1].tabela.frete;
+        cpf.textContent = market_place[1].tabela.cpf;
+        cnpj.textContent = market_place[1].tabela.cnpj;
+        nf.textContent = market_place[1].tabela.nf;
+        link.href = market_place[1].tabela.link;
+        observacao.textContent = market_place[1].tabela.obs;
+        handleButtonClick();
+    });
+
+    botao_3.addEventListener('click', function() {
+        imagem.src = 'image/dafiti.jpg';
+        titulo.forEach(function(titulo){
+            titulo.textContent = 'Dafiti';
+        });
+        descricao.textContent = market_place[2].breve_descricao;
+        taxa_1.textContent = market_place[2].tabela.taxa_1;
+        taxa_2.textContent = market_place[2].tabela.taxa_2;
+        envio.textContent = market_place[2].tabela.envio;
+        frete.textContent = market_place[2].tabela.frete;
+        cpf.textContent = market_place[2].tabela.cpf;
+        cnpj.textContent = market_place[2].tabela.cnpj;
+        nf.textContent = market_place[2].tabela.nf;
+        link.href = market_place[2].tabela.link;
+        observacao.textContent = market_place[2].tabela.obs;
+        handleButtonClick();
+    });
+
+
+    botao_4.addEventListener('click', function() {
+        imagem.src = 'image/elo.jpg';
+        titulo.forEach(function(titulo){
+            titulo.textContent = 'Elo';
+        });
+        descricao.textContent = market_place[3].breve_descricao;
+        taxa_1.textContent = market_place[3].tabela.taxa_1;
+        taxa_2.textContent = market_place[3].tabela.taxa_2;
+        envio.textContent = market_place[3].tabela.envio;
+        frete.textContent = market_place[3].tabela.frete;
+        cpf.textContent = market_place[3].tabela.cpf;
+        cnpj.textContent = market_place[3].tabela.cnpj;
+        nf.textContent = market_place[3].tabela.nf;
+        link.href = market_place[3].tabela.link;
+        observacao.textContent = market_place[3].tabela.obs;
+        handleButtonClick();
+    });
+
+    botao_5.addEventListener('click', function() {
+        imagem.src = 'image/magalu.jpg';
+        titulo.forEach(function(titulo){
+            titulo.textContent = 'Magazine Luiza';
+        });
+        descricao.textContent = market_place[4].breve_descricao;
+        taxa_1.textContent = market_place[4].tabela.taxa_1;
+        taxa_2.textContent = market_place[4].tabela.taxa_2;
+        envio.textContent = market_place[4].tabela.envio;
+        frete.textContent = market_place[4].tabela.frete;
+        cpf.textContent = market_place[4].tabela.cpf;
+        cnpj.textContent = market_place[4].tabela.cnpj;
+        nf.textContent = market_place[4].tabela.nf;
+        link.href = market_place[4].tabela.link;
+        observacao.textContent = market_place[4].tabela.obs;
+        handleButtonClick();
+    });
+
+    botao_6.addEventListener('click', function() {
+        imagem.src = 'image/ml.jpg';
+        titulo.forEach(function(titulo){
+            titulo.textContent = 'Mercado Livre';
+        });
+        descricao.textContent = market_place[5].breve_descricao;
+        taxa_1.textContent = market_place[5].tabela.taxa_1;
+        taxa_2.textContent = market_place[5].tabela.taxa_2;
+        envio.textContent = market_place[5].tabela.envio;
+        frete.textContent = market_place[5].tabela.frete;
+        cpf.textContent = market_place[5].tabela.cpf;
+        cnpj.textContent = market_place[5].tabela.cnpj;
+        nf.textContent = market_place[5].tabela.nf;
+        link.href = market_place[5].tabela.link;
+        observacao.textContent = market_place[5].tabela.obs;
+        handleButtonClick();
+
+    });
+
+    botao_7.addEventListener('click', function() {
+       imagem.src = 'image/shope.jpg';
+       titulo.forEach(function(titulo){
+        titulo.textContent = 'Shope';
+    });
+        descricao.textContent = market_place[6].breve_descricao;
+        taxa_1.textContent = market_place[6].tabela.taxa_1;
+        taxa_2.textContent = market_place[6].tabela.taxa_2;
+        envio.textContent = market_place[6].tabela.envio;
+        frete.textContent = market_place[6].tabela.frete;
+        cpf.textContent = market_place[6].tabela.cpf;
+        cnpj.textContent = market_place[6].tabela.cnpj;
+        nf.textContent = market_place[6].tabela.nf;
+        link.href = market_place[6].tabela.link;
+        observacao.textContent = market_place[6].tabela.obs;
+        handleButtonClick();
+    });
+
+    botao_8.addEventListener('click', function() {
+        handleButtonClick_graph();
+    });
+
+});
